611d5a33122750bdf8064ac44ca74212a7361b6c
Judy Zhu <judy@MacBook-Air.local>
2023-09-13 09:44:48 -0400

    added function

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,5 +1,11 @@
 public class Reduce {
     public static void main(String[] args) {
-
+        int i = 100;
+        int count = 0;
+        while (i > 0) {
+            count++;
+            i--;
+        }
+        System.out.println(count);
     }
 }
